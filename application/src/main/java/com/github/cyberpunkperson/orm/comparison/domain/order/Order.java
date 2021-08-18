cf3e4d6c47f5122db22625984601873aed3249b8
Andrew Belunov <cyberpunk.person@gmail.com>
2021-08-18 12:01:27 +0300

    Service tier implementation

--- application/src/main/java/com/github/cyberpunkperson/orm/comparison/domain/order/Order.java
+++ application/src/main/java/com/github/cyberpunkperson/orm/comparison/domain/order/Order.java
@@ -1,25 +1,21 @@
 package com.github.cyberpunkperson.orm.comparison.domain.order;
 
-import com.github.cyberpunkperson.orm.comparison.model.Address;
+import com.github.cyberpunkperson.orm.comparison.domain.item.Item;
+import com.github.cyberpunkperson.orm.comparison.domain.value.Address;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.MappedCollection;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
-class Order {
+public class Order {
 
-    @Id
     private UUID id;
 
     private UUID userId;
 
-    @MappedCollection(idColumn = "address_id")
     private Address address;
 
-    @MappedCollection(idColumn = "id")
     private List<Item> items;
 
 }
